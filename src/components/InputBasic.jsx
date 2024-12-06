782fb4bc81c12667b3dd45e2353a431053783fa0
NguyentThanhAn <nguyenthanhanqt@gmail.com>
2024-12-06 12:00:25 +0700

    complete UI for view task and waiting for API integration

--- src/components/InputBasic.jsx
+++ src/components/InputBasic.jsx
@@ -1,15 +1,34 @@
 import { DatePicker, Input, Select } from 'antd';
 import dayjs from 'dayjs';
 import Button from '~/components/Button';
+import { inputKey } from '~/routes/inputReducer';
 
-function InputBasic() {
+function InputBasic({ dispatch, state }) {
+    const { title, type, language, date, status } = state;
     return (
         <>
             <div className="flex justify-between space-x-3">
-                <Input placeholder="Add chapter name" className="lg:!w-[60%]" />
+                <Input
+                    value={title}
+                    onChange={(e) => {
+                        dispatch({
+                            type: inputKey.ADDTITLE,
+                            value: e.target.value,
+                        });
+                    }}
+                    placeholder="Add chapter name"
+                    className="lg:!w-[60%]"
+                />
                 <Select
                     className="!flex-1"
-                    defaultValue={''}
+                    onChange={(e) => {
+                        dispatch({
+                            type: inputKey.ADDTYPE,
+                            value: e,
+                        });
+                    }}
+                    value={type}
+                    defaultValue={type}
                     options={[
                         {
                             value: '',
@@ -27,7 +46,11 @@ function InputBasic() {
                 />
                 <Select
                     className="!flex-1"
-                    defaultValue={''}
+                    defaultValue={language}
+                    onChange={(e) => {
+                        dispatch({ type: inputKey.ADDLANGUAGE, value: e });
+                    }}
+                    value={language}
                     options={[
                         {
                             value: '',
@@ -43,16 +66,33 @@ function InputBasic() {
                         },
                     ]}
                 />
-                <Button>Find</Button>
+                <Button
+                    onClick={() => {
+                        dispatch({ type: inputKey.CLEAR });
+                    }}
+                >
+                    Find
+                </Button>
             </div>
             <div className="space-x-3">
                 <DatePicker
+                    onChange={(e) => {
+                        dispatch({
+                            type: inputKey.ADDDATE,
+                            value: e?.$d || new Date(),
+                        });
+                    }}
                     format={'MM/DD/YYYY'}
-                    defaultValue={dayjs(new Date().toLocaleDateString('en-GB'), 'MM/DD/YYYY')}
+                    value={dayjs(date.toLocaleDateString('en-GB'), 'MM/DD/YYYY')}
+                    // defaultValue={dayjs(date.toLocaleDateString('en-GB'), 'MM/DD/YYYY')}
                 />
                 <Select
                     className="!w-[150px]"
-                    defaultValue={''}
+                    defaultValue={status}
+                    onChange={(e) => {
+                        dispatch({ type: inputKey.ADDSTATUS, value: e });
+                    }}
+                    value={status}
                     options={[
                         {
                             value: '',
@@ -67,7 +107,7 @@ function InputBasic() {
                             label: 'On going',
                         },
                         {
-                            value: 2,
+                            value: 3,
                             label: 'Over deadline',
                         },
                     ]}
