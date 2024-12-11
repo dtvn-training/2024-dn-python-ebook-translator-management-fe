5c0f7e4df25b09589778d2617d90c49582f5e4f3
Annn <Annndang@gmail.com>
2024-12-11 17:01:56 +0700

    [PB16-5]- Update Create Task

--- src/pages/Create_task.jsx
+++ src/pages/Create_task.jsx
@@ -1,131 +1,151 @@
-import React, { useState } from 'react';
-import { Transfer, Button, Dropdown, Space, Input, DatePicker } from 'antd';
-const { RangePicker } = DatePicker;
+import React, { useState, useEffect } from 'react';
+import { Transfer, Button, Select, Space, Input, DatePicker, Alert } from 'antd';
+import { get } from '~/db';
+import warning from 'antd/es/_util/warning';
+
 const onOk = (value) => {
     console.log('onOk: ', value);
 };
 
-const items = [
-    {
-        key: '1',
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-                Ebook 1
-            </a>
-        ),
-    },
-    {
-        key: '2',
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
-                Ebook 2
-            </a>
-        ),
-    },
-    {
-        key: '3',
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-                Ebook 3
-            </a>
-        ),
-    },
-];
-
-const mockData = Array.from({
-    length: 5,
-}).map((_, i) => ({
-    key: i.toString(),
-    title: `Chapter ${i + 1}`,
-    description: `description of content${i + 1}`,
-}));
-
-const oriTargetKeys = mockData.filter((item) => Number(item.key) % 3 > 1).map((item) => item.key);
 const Create_task = () => {
-    const [targetKeys, setTargetKeys] = useState(oriTargetKeys);
+    const [books, setBooks] = useState([]);
+    const [chapters, setChapters] = useState([]);
+    const [selectedBook, setSelectedBook] = useState(null);
+    const [targetKeys, setTargetKeys] = useState([]);
     const [selectedKeys, setSelectedKeys] = useState([]);
-    const handleChange = (newTargetKeys, direction, moveKeys) => {
-        setTargetKeys(newTargetKeys);
-        console.log('targetKeys: ', newTargetKeys);
-        console.log('direction: ', direction);
-        console.log('moveKeys: ', moveKeys);
+    const [error, setError] = useState('');
+    const [deadline, setDeadline] = useState(null);
+    const [salary, setSalary] = useState('');
+
+    const onChange = (nextTargetKeys, direction, moveKeys) => {
+        console.log('targetKeys:', nextTargetKeys);
+        setTargetKeys(nextTargetKeys);
     };
-    const handleSelectChange = (sourceSelectedKeys, targetSelectedKeys) => {
+
+    const onSelectChange = (sourceSelectedKeys, targetSelectedKeys) => {
+        console.log('sourceSelectedKeys:', sourceSelectedKeys);
         setSelectedKeys([...sourceSelectedKeys, ...targetSelectedKeys]);
-        console.log('sourceSelectedKeys: ', sourceSelectedKeys);
-        console.log('targetSelectedKeys: ', targetSelectedKeys);
     };
-    const handleScroll = (direction, e) => {
+
+    const onScroll = (direction, e) => {
         console.log('direction:', direction);
         console.log('target:', e.target);
     };
+
+    const handleFindBooks = async () => {
+        try {
+            const res_book = await get('/books');
+            if (res_book.status === 200 || res_book.statusText === 'OK') {
+                setBooks(res_book.data);
+                if (res_book.data.length > 0) {
+                    const firstBook = res_book.data[0];
+                    setSelectedBook(firstBook.book_id);
+                }
+            }
+        } catch (error) {
+            console.error('Error fetching books:', error);
+        }
+    };
+
+    const fetchChapters = async (bookId) => {
+        try {
+            const res_chapter = await get(`/chapters/${bookId}`);
+            if (res_chapter.status === 200 || res_chapter.statusText === 'OK') {
+                const chapterData = res_chapter.data.map((chapter) => ({
+                    key: chapter.chapter_id.toString(),
+                    title: chapter.chapter_title,
+                }));
+                setChapters(chapterData);
+                setTargetKeys([]);
+                setSelectedKeys([]);
+            }
+        } catch (error) {
+            console.error('Error fetching chapters:', error);
+        }
+    };
+
+    useEffect(() => {
+        handleFindBooks();
+    }, []);
+
+    const handleTransfer = () => {
+        const currentTime = new Date();
+        if (!selectedBook || targetKeys.length === 0) {
+            setError('Please chose least a book and a chapter');
+            return;
+        }
+        if (isNaN(deadline)) {
+            setError('Please input deadline!');
+            return;
+        }
+        if (deadline <= currentTime) {
+            setError('Deadline must be greater now!');
+            return;
+        }
+        if (isNaN(salary) || salary <= 0) {
+            setError('Salary not empty must be greater 0!');
+            return;
+        }
+        setError('');
+    };
+
     return (
-        <>
+        <div>
             <Space direction="vertical">
-                <Space wrap>
-                    <Dropdown
-                        className="mb-8"
-                        menu={{
-                            items,
-                        }}
-                        placement="bottomLeft"
-                        arrow={{
-                            pointAtCenter: true,
-                        }}
-                    >
-                        <Button className="bg-gray-200 w-80 space-x-52">
-                            <h6>Ebook</h6>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1"
-                                stroke="currentColor"
-                                class="size-4"
-                            >
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-                            </svg>
-                        </Button>
-                    </Dropdown>
-                </Space>
+                <Select
+                    showSearch
+                    style={{ width: 300 }}
+                    placeholder="Select a Book"
+                    optionFilterProp="label"
+                    filterSort={(optionA, optionB) =>
+                        (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+                    }
+                    options={books.map((book) => ({
+                        value: book.book_id,
+                        label: book.book_title,
+                    }))}
+                    onChange={(value) => {
+                        setSelectedBook(value);
+                        fetchChapters(value);
+                    }}
+                />
             </Space>
+
             <Transfer
-                dataSource={mockData}
-                titles={['Chapter', 'Target']}
+                dataSource={chapters}
+                titles={['Available Chapters', 'Selected Chapters']}
                 targetKeys={targetKeys}
                 selectedKeys={selectedKeys}
-                onChange={handleChange}
-                onSelectChange={handleSelectChange}
-                onScroll={handleScroll}
+                onChange={onChange}
+                onSelectChange={onSelectChange}
+                onScroll={onScroll}
                 render={(item) => item.title}
-                oneWay
-                style={{
-                    marginBottom: 16,
-                }}
+                className="mt-8"
             />
+
             <div>
-                <h4 className="mt-4 mb-4">Chapter 4</h4>
+                <h4 className="mt-4 mb-4">
+                    Notification: {error && <Alert message={error} type="error" showIcon className="mt-2" />}{' '}
+                </h4>
                 Deadline:
-                <RangePicker
-                    className="mb-4 ml-4"
-                    showTime={{
-                        format: 'HH:mm',
-                    }}
-                    format="YYYY-MM-DD HH:mm"
-                    onChange={(value, dateString) => {
-                        console.log('Selected Time: ', value);
-                        console.log('Formatted Selected Time: ', dateString);
-                    }}
-                    onOk={onOk}
-                />
-                <br></br>
+                <Space direction="vertical" size={12} className="mb-4">
+                    <DatePicker
+                        showTime
+                        onChange={(value, dateString) => {
+                            console.log('Selected Time: ', value);
+                            console.log('Formatted Selected Time: ', dateString);
+                        }}
+                        onOk={onOk}
+                    />
+                </Space>
+                <br />
                 Salary: <Input placeholder="Salary" className="mb-4 ml-4" style={{ width: '8%' }} />
-                <br></br>
-                <Button type="primary" style={{ width: '10%' }}>
+                <br />
+                <Button type="primary" style={{ width: '10%' }} onClick={handleTransfer}>
                     Submit
                 </Button>
             </div>
-        </>
+        </div>
     );
 };
 
