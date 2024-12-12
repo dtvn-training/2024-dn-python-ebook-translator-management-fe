bb62e492b7fa3737e0363c0d72de92c7c28b5f1e
NguyentThanhAn <nguyenthanhanqt@gmail.com>
2024-12-12 11:56:16 +0700

    implement upload ebook

--- src/routes/customPage.js
+++ src/routes/customPage.js
@@ -1,5 +1,7 @@
 import { lazy } from 'react';
 import { pathname } from './pathname';
+import { UPLOAD_EBOOK } from '~/utils/constants';
+const UploadEbook = lazy(() => import('~/pages/UploadEbook'));
 const TaskManagement = lazy(() => import('~/pages/TaskManagement'));
 const Home = lazy(() => import('~/pages/Home'));
 
@@ -7,3 +9,5 @@ export const memberPage = [
     { pathname: pathname.HOME_PAGE, Element: Home },
     { pathname: pathname.TASK_MANAGEMENT, Element: TaskManagement },
 ];
+
+export const adminPage = [{ pathname: UPLOAD_EBOOK, Element: UploadEbook }];
