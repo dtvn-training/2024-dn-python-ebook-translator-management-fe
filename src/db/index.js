c9f310520061b505822ce279dc089b7609f168dd
NguyentThanhAn <nguyenthanhanqt@gmail.com>
2024-12-12 11:56:16 +0700

    implement upload ebook

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 const config = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'http://localhost:5000/api',
     headers: {
         'Content-Type': 'application/json',
     },
     timeout: 5000,
     withCredentials: true,
+    timeout: 10000,
 });
 
 export const get = async (url, options) => {
     try {
         const response = await config.get(url, options);
-        return response.data;
+        return response;
     } catch (error) {
         throw error;
     }
@@ -20,7 +21,7 @@ export const get = async (url, options) => {
 export const post = async (url, data, options) => {
     try {
         const response = await config.post(url, data, options);
-        return response.data;
+        return response;
     } catch (error) {
         throw error;
     }
@@ -29,7 +30,7 @@ export const post = async (url, data, options) => {
 export const put = async (url, data, options) => {
     try {
         const response = await config.put(url, data, options);
-        return response.data;
+        return response;
     } catch (error) {
         throw error;
     }
