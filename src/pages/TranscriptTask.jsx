e3f9cb00fa657f9c901b93585190ea4f00f58900
NguyentThanhAn <nguyenthanhanqt@gmail.com>
2024-12-17 11:26:52 +0700

    completed UI for translation page

--- src/pages/TranscriptTask.jsx
+++ src/pages/TranscriptTask.jsx
@@ -1,4 +1,11 @@
+import { Button, Upload } from 'antd';
+import { useState } from 'react';
+import { IoCloudUploadOutline } from 'react-icons/io5';
+import ViewComment from '~/components/ViewComment';
+import ButtonCustom from '~/components/Button';
+
 function TranscriptTask() {
+    const [translationFile, setTranslationFile] = useState([]);
     return (
         <div className="space-y-2">
             <p>Chapter's name: Pokemon</p>
@@ -37,6 +44,43 @@ function TranscriptTask() {
                     <h2 className="text-[20px]">Translate</h2>
                     <textarea placeholder="Add your transcript " className="w-full h-[500px] resize-none"></textarea>
                 </div>
+            </div>
+            <div className="flex items-start space-x-3 h-[70px]">
+                <h2 className="text-base">Upload Translation:</h2>
+                <Upload
+                    onRemove={() => {
+                        setTranslationFile([]);
+                    }}
+                    beforeUpload={(e) => {
+                        console.log(e);
+                        setTranslationFile([e]);
+                        return false;
+                    }}
+                    fileList={translationFile}
+                >
+                    <Button icon={<IoCloudUploadOutline />}>Upload</Button>
+                </Upload>
+            </div>
+            <div className="space-y-4 xl:w-1/2">
+                <h2 className="text-xl">Comment</h2>
+                <div className="pl-2 space-y-4">
+                    <ViewComment
+                        author={'Nguyen Thanh An - manager'}
+                        content={
+                            'Can co mot vai chinh sua Can co mot vai chinh sua Can co mot vai chinh sua Can co mot vai chinh sua Can co mot vai chinh sua Can co mot vai chinh sua Can co mot vai chinh sua'
+                        }
+                        isComfirm={true}
+                    />
+                    <ViewComment
+                        author={'Nguyen Thanh An - manager'}
+                        content={'Can co mot vai chinh sua'}
+                        isComfirm={false}
+                    />
+                </div>
+            </div>
+            <div className="pt-5 space-x-4">
+                <ButtonCustom blue>Save</ButtonCustom>
+                <ButtonCustom green>Completed</ButtonCustom>
             </div>
         </div>
     );
