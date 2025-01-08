33267d797e416a0163c1d6ab1a89f1a565c4d90a
NguyentThanhAn <nguyenthanhanqt@gmail.com>
2025-01-08 17:02:57 +0700

    implement ui for 404 page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
         extend: {
             colors: {
                 default_bt: '#D9D9D9',
+                not_found: '#122638',
+                button_404: '#1A3F55',
+                text_404_0: '#1B3044',
+                text_404_100: '#122638',
             },
         },
     },
